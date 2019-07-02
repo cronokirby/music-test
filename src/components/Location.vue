cbb75883adddca13a55a1eb4f7b8a53cd09b42e6
Lúcás Meier <cronokirby@gmail.com>
2019-07-02 16:39:52 +0200

    Add distance calculation

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -2,6 +2,15 @@
   <div class="hello">
     <p>Latitude: {{ position ? position.latitude : 'unknown' }}</p>
     <p>Longitude: {{ position ? position.longitude : 'unknown' }}</p>
+    <div>
+      <h4>Latitude input:</h4>
+      <input v-model.number="lat">
+    </div>
+    <div>
+      <h4>Longitude input:</h4>
+      <input v-model.number="long">
+    </div>
+    <p>Distance: {{distance}}</p>
   </div>
 </template>
 
@@ -10,11 +19,16 @@ import { Position } from "../geo.js";
 
 export default {
   data() {
-    return { position: undefined };
+    return { position: undefined, lat: 0, long: 0 };
+  },
+  computed: {
+    distance() {
+      const target = new Position(this.lat, this.long);
+      return this.position ? this.position.distance(target) : 0;
+    }
   },
   async created() {
     this.position = await Position.current();
-    console.log(this.position);
   }
 };
 </script>
--- src/geo.js
+++ src/geo.js
@@ -1,15 +1,34 @@
 export const HAS_GEO = "geolocation" in navigator;
 
+function deg2rad(deg) {
+    return deg * Math.PI / 180;
+}
+
 export class Position {
-  constructor(position) {
-    this.latitude = position.coords.latitude;
-    this.longitude = position.coords.longitude;
+  constructor(lat, long) {
+    this.latitude = lat;
+    this.longitude = long;
+  }
+
+  static fromNavPosition(position) {
+      return new Position(position.coords.latitude, position.coords.longitude);
   }
 
   static async current() {
     if (!("geolocation" in navigator)) return null;
     return new Promise(resolve =>
       navigator.geolocation.getCurrentPosition(resolve)
-    ).then(x => new Position(x));
+    ).then(Position.fromNavPosition);
+  }
+
+  distance(that) {
+      const R = 6371;
+      const dLat = deg2rad(that.latitude - this.latitude);
+      const dLon = deg2rad(that.longitude - this.longitude);
+      const sinHalfDLat = Math.sin(dLat / 2);
+      const sinHalfDLon = Math.sin(dLon / 2);
+      const a = sinHalfDLat * sinHalfDLat + Math.cos(deg2rad(this.latitude)) * Math.cos(deg2rad(that.latitude)) * sinHalfDLon * sinHalfDLon;
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      return R * c;
   }
 }
